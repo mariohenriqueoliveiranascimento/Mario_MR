3a89688268016c0d424adaf1b1cb18fe4d80ceec
mario henrique <mario5.8.2.8.10@hotmail.com>
2018-07-21 17:16:13 -0300

    Curso CSharp - 1

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola, mundo. Projeto no visual studio!");
+
+            Console.ReadLine();
+
+            Console.WriteLine("A execução acabou. Tecle enter para continuar. . .");
+
+            
+        }
+    }
+}
